11f3bc5310b09fb62588d5a0d5c16a1aa6557ea5
tommyrharper <thomasrobertharper@gmail.com>
2021-03-23 18:18:35 +0000

    Fetch properties in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,22 @@ import './App.scss';
 import Header from '../Header';
 import SortAndFilter from '../SortAndFilter';
 import PropertyListing from '../PropertyListing';
+import useFetch from '../hooks/useFetch';
+
+const API_ENDPOINT = 'http://localhost:3000/api/properties';
 
 const App = () => {
+    const [response, loading, hasError] = useFetch(API_ENDPOINT);
+    const showPropertyListing = response && !loading && !hasError;
+
     return (
         <div className="App">
             <Header/>
             <main>
                 <SortAndFilter/>
-                <PropertyListing/>
+                { showPropertyListing &&
+                  <PropertyListing properties={response} />
+                }
             </main>
         </div>
     );
